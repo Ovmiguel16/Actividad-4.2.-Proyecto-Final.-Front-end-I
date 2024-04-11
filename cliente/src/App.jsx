a81ef90aeb5397533f1a7b5f2046cc3071eec824
richardsiv <richardsiverioww@gmail.com>
2024-04-11 13:29:14 -0400

    Instalando librerias necesarias

---
+++ cliente/src/App.jsx
@@ -0,0 +1,58 @@
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import "./App.scss";
+import Login from "./components/Auth/Login";
+import Registro from "./components/Auth/Registro";
+import AuthTemplate from "./components/Auth/AuthTemplate";
+import { ContextProvider } from "./context/GlobalContext";
+import Datos from "./components/Escritorio/Datos";
+import Layout from "./components/Template/Layout";
+import Encuestas from "./components/Encuestas/Encuestas";
+import CrearEncuesta from "./components/Encuestas/CrearEncuesta";
+import { useEffect } from "react";
+import Perfil from "./components/Perfil/Perfil";
+import Lista from "./components/Usuarios/Lista";
+import Resultados from "./components/Encuestas/Resultados";
+import ResultadoEncuesta from "./components/Encuestas/ResultadoEncuesta";
+
+function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/login");
+    }
+  }, [location]);
+
+  return (
+    <ContextProvider>
+      
+      <Routes>
+        <Route path="/" element={<AuthTemplate />}>
+          <Route path="login" element={<Login />} />
+          <Route path="registro" element={<Registro />} />
+        </Route>
+
+        <Route path="/escritorio" element={<Layout />}>
+          <Route path="inicio" element={<Datos />} />
+          <Route path="encuestas" element={<Encuestas />} />
+          <Route path="encuestas/crear" element={<CrearEncuesta />} />
+          <Route path="resultados" element={<Resultados />} />
+          <Route path="resultados/:idEncuesta" element={<ResultadoEncuesta />} />
+          <Route path="perfil/:idUsuario" element={<Perfil />} />
+          <Route path="usuarios" element={<Lista />} />
+        </Route>
+
+        <Route
+          path="/*"
+          element={
+            <div className="w-100 vh-100 d-flex flex-column justify-content-center">
+              <h1 className="text-center">Error 404</h1>
+            </div>
+          }
+        />
+      </Routes>
+    </ContextProvider>
+  );
+}
+
+export default App;
